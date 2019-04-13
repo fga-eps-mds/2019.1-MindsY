ed3fbfa3c83c686a8415a92c52cb08ed3da414b2
pedroMiranda7410 <pmiranda7410@gmail.com>
2019-04-13 15:52:15 -0300

    Estruturação das pastas para cada parte envolvendo
    Co-authored by: Natilorens <nathalialorenacardoso@gmail.com>
    
    Please enter the commit message for your changes. Lines starting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { SectionHomeComponent } from './layouts/section-home/section-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    SectionHomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/layouts/section-home/section-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-home',
+  templateUrl: './section-home.component.html',
+  styleUrls: ['./section-home.component.css']
+})
+export class SectionHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
