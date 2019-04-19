be909d39b5e5b956004f772bf9e851fdfd8d8d53
deborapvilela <deborapvilela@gmail.com>
2019-04-19 07:37:59 -0300

    Adicionando versão inicial componente CREATE
    Co-authored-by: Nathlorens <nathalialorenacardoso@gmail.com>
    Co-authored-by: pedroMiranda7410 <pmiranda7410@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './user/login/login.component';
+import { CreateComponent } from './user/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
