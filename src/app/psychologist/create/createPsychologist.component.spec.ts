ffb70a9b91a8745d9524071e871f57d474986665
joao4018 <joao4018@gmail.com>
2019-05-12 22:46:57 -0300

    Ajustando spect createPsychologist

--- src/app/psychologist/create/createPsychologist.component.spec.ts
+++ src/app/psychologist/create/createPsychologist.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CreatePsychologistComponent } from './create.component';
+import { CreatePsychologistComponent } from './createPsychologist.component';
 
 describe('CreatePsychologistComponent', () => {
   let component: CreatePsychologistComponent;
