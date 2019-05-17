d825c23127038a029a2b122c3b1d15a758de6873
filypsdias <lipao.dias@hotmail.com>
2019-05-16 21:54:00 -0300

    Inserindo teste do componente de edição de pacient
    Co-authored-by: joao4018 <joao4018@gmail.com>
    Co-authored-by: deborapvilela <deborapvilela@gmail.com>

--- src/app/patient/edit/edit.component.spec.ts
+++ src/app/patient/edit/edit.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditPacientComponent } from './edit.component';
+import { PatientService } from 'src/app/services';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { TextMaskModule } from 'angular2-text-mask';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('EditPacientComponent', () => {
   let component: EditPacientComponent;
@@ -8,7 +13,11 @@ describe('EditPacientComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditPacientComponent ]
+      declarations: [ EditPacientComponent ],
+      imports: [HttpClientModule, FormsModule, TextMaskModule, RouterTestingModule],
+      providers: [
+        PatientService
+      ]
     })
     .compileComponents();
   }));
