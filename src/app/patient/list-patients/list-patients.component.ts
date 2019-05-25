946c55da5858df32c8d0011d90ce6e6a8b900d22
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2019-05-25 15:30:29 -0300

    Retirando código sobre tabela dinâmica

--- src/app/patient/list-patients/list-patients.component.ts
+++ src/app/patient/list-patients/list-patients.component.ts
@@ -3,7 +3,7 @@ import { PatientService } from 'src/app/services/index';
 import { Patient } from 'src/app/models';
 
 import * as $ from 'jquery';
-import 'datatables.net'
+//import 'datatables.net'
 
 @Component({
   selector: 'app-list-patients',
@@ -12,34 +12,34 @@ import 'datatables.net'
 })
 
 export class ListPatientsComponent implements OnInit {
-  
+
   patients: Patient[];
   columns: string[];
-  
-  @ViewChild('dataTable') table;
-  dataTable: any;
+
+  //@ViewChild('dataTable') table;
+  //dataTable: any;
 
   constructor(private patientService: PatientService, private chRef: ChangeDetectorRef) { }
 
-  template:`  
+  template:`
     ASsafsdf
     asfdasdf
     asdfa
     sdfas
-    fasf  
+    fasf
   `
-  
+
   ngOnInit() {
-    
+
     //TODO: Pegar is do psicólogo que está logado.
     this.patients = this.patientService.getAllPatients('01-122454');
     this.columns = ['name', 'registry_number_pat', 'number'];
-  
-    this.dataTable = $(this.table.nativeElement);
-    this.dataTable.DataTable();    
+
+    //this.dataTable = $(this.table.nativeElement);
+    //this.dataTable.DataTable();
 
   }
 
-  
+
 
 }
