7fea939e09cb2bdc200dfdfe74f445414d624e67
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2019-06-13 23:08:13 -0300

    Aprimorando página de editar avaliação
    Co-authored-by: dansousamelo <daniel_sousa.unb@outlook.com>

--- src/app/patient/report/edit-report/edit-report.component.ts
+++ src/app/patient/report/edit-report/edit-report.component.ts
@@ -12,7 +12,7 @@ declare var $: any;
 @Component({
   selector: 'app-edit-report',
   templateUrl: './edit-report.component.html',
-  styleUrls: ['./edit-report.component.css'],
+  styleUrls: ['./edit-report.component.scss'],
   providers: [ReportService],
 })
 export class EditReportComponent implements OnInit {
@@ -45,13 +45,31 @@ export class EditReportComponent implements OnInit {
     );    
     
     $('#summernote').summernote();    
-
+    $(".check-icon").hide();
   }
 
   functionSummernote(){
     var markupStr = this.report.anamnese.toString(); 
     // $('.note-editable #anamnese').html('<textarea ngModel name="anamnese" #anamnese="ngModel" cols="30" rows="10" >'+ markupStr +'</textarea>');
   }
+
+  initSubmit(){
+      $(".check-icon").hide();
+      setTimeout(function () {
+        $(".check-icon").show();
+      }, 10);
+  }
+
+  initModal(){
+
+    // alert('The paragraph was clicked<br><button class="btn btn-primary" value="Atualizar">Atualizar</button>');
+    $('#dialog_id').dialog();
+}
+
+closeModal(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+}
 
   onSubmit(f: NgForm){
    return this.reportService.updateReport(f)
