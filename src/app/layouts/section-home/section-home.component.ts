df64e20046a07bc44e6f47639c2d6164a6983459
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2019-06-24 22:40:24 -0300

    Arrumando section-home

--- src/app/layouts/section-home/section-home.component.ts
+++ src/app/layouts/section-home/section-home.component.ts
@@ -19,6 +19,7 @@ export class SectionHomeComponent implements OnInit {
   currentUser: any;
   currentUserSubscription: Subscription;
   users: Psychologist[] = [];
+  
 
   constructor(
     private authenticationService: AuthenticationService
@@ -61,64 +62,6 @@ export class SectionHomeComponent implements OnInit {
               });
           });
       }
-
-      //line
-      var canvas : any = document.getElementById("lineChart");
-      var ctxL = canvas.getContext('2d');
-      var myLineChart = new Chart(ctxL, {
-        type: 'line',
-        data: {
-          labels: ["Janeiro", "Fevereiro", "Março", "Abriu", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro","Dezembro"],
-          datasets: [{
-              label: "Depressão Clínica",
-              data: [65, 59, 80, 81, 56, 55, 40, 22, 12, 20, 55, 3],
-              backgroundColor: [
-                'rgba(105, 0, 132, .2)',
-              ],
-              borderColor: [
-                'rgba(200, 99, 132, .7)',
-              ],
-              borderWidth: 2
-            },
-            {
-              label: "Transtorno Bipolar",
-              data: [28, 48, 40, 19, 86, 27, 90,33, 21, 23,35, 7],
-              backgroundColor: [
-                'rgba(0, 137, 132, .2)',
-              ],
-              borderColor: [
-                'rgba(0, 10, 130, .7)',
-              ],
-              borderWidth: 2
-            },
-            {
-              label: "Esquizofrenia",
-              data: [11, 33, 21, 3, 6,7, 9, 33, 52, 60, 22, 52],
-              backgroundColor: [
-                'rgba(92.5, 47.5, 0, .2)',
-              ],
-              borderColor: [
-                'rgba(92.5, 47.5, 0, .7)',
-              ],
-              borderWidth: 2
-            },
-            {
-              label: "Doença EXTRA...",
-              data: [1, ,2 ,3 ,4 ,5 ,6 ,7, 8, 9, 10, 11, 12],
-              backgroundColor: [
-                'rgba(13.3, 77.3, 0, .2)',
-              ],
-              borderColor: [
-                'rgba(13.3, 77.3, 0, .7)',
-              ],
-              borderWidth: 2
-            }
-          ]
-        },
-        options: {
-          responsive: true
-        }
-      });
 
 
       //line
